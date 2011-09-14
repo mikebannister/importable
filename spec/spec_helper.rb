b0169f5d0d29ff1cd2df0b1a987bc7d7ac3694a0
Mike Bannister <mikebannister@gmail.com>
2011-09-14 17:52:52 -0400

    rspec and capybara, removed testunit

---
+++ spec/spec_helper.rb
@@ -0,0 +1,16 @@
+# Configure Rails Environment
+ENV["RAILS_ENV"] = "test"
+
+require File.expand_path("../dummy/config/environment.rb", __FILE__)
+require 'rspec/rails'
+require 'capybara/rspec'
+
+Rails.backtrace_cleaner.remove_silencers!
+
+# Load support files
+Dir["#{File.dirname(__FILE__)}/support/**/*.rb"].each { |f| require f }
+
+RSpec.configure do |config|
+  config.mock_with :rspec
+  config.use_transactional_fixtures = false
+end
