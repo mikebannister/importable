fe7a6fe3d4f5d8f8639d697b9bb9c6939d9872ec
Mike Bannister <mikebannister@gmail.com>
2011-09-15 22:42:41 -0400

    fixed issues with choosing default sheet, also added spork

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -1,20 +1,31 @@
-# Configure Rails Environment
-ENV["RAILS_ENV"] = "test"
+require 'spork'
 
-require File.expand_path("../dummy/config/environment.rb", __FILE__)
-require 'rspec/rails'
-require 'capybara/rspec'
+Spork.prefork do
+  ENV["RAILS_ENV"] = "test"
 
-Rails.backtrace_cleaner.remove_silencers!
+  require File.expand_path("../dummy/config/environment.rb", __FILE__)
+  require 'rspec/rails'
+  require 'capybara/rspec'
 
-# Load support files
-Dir["#{File.dirname(__FILE__)}/support/**/*.rb"].each { |f| require f }
+  Rails.backtrace_cleaner.remove_silencers!
 
-RSpec.configure do |config|
-  config.mock_with :rspec
-  config.use_transactional_fixtures = false
+  # Load support files
+  Dir["#{File.dirname(__FILE__)}/support/**/*.rb"].each { |f| require f }
+
+  RSpec.configure do |config|
+    config.mock_with :rspec
+    config.use_transactional_fixtures = false
+  end
+
+  def support_file(name)
+    File.expand_path("spec/support/#{name}")
+  end
 end
 
-def support_file(name)
-  File.expand_path("spec/support/#{name}")
+Spork.each_run do
+  if Spork.using_spork?
+    load 'Sporkfile' if File.exists? "Sporkfile"
+  end
+   
+  Dummy::Application.reload_routes!
 end
