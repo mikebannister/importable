9f6e4229eccced74f9a60db47ede75bcc5ad6444
Mike Bannister <mikebannister@gmail.com>
2011-09-21 23:49:47 -0400

    generator can now generate model based views

--- lib/generators/importable/views/views_generator.rb
+++ lib/generators/importable/views/views_generator.rb
@@ -1,17 +1,26 @@
 module Importable
   class ViewsGenerator < Rails::Generators::NamedBase
     source_root File.expand_path('../../../../../app/views/importable', __FILE__)
-    argument :name, :type => :string, :default => 'views'
+    argument :name, :type => :string, :default => ''
 
     def copy_view_files
-      empty_directory "app/views/importable/spreadsheets"
-      copy_file "spreadsheets/_actions.erb", "app/views/importable/spreadsheets/_actions.erb"
-      copy_file "spreadsheets/_choose_worksheet_step.erb", "app/views/importable/spreadsheets/_choose_worksheet_step.erb"
-      copy_file "spreadsheets/_errors.erb", "app/views/importable/spreadsheets/_errors.erb"
-      copy_file "spreadsheets/_form.html.erb", "app/views/importable/spreadsheets/_form.html.erb"
-      copy_file "spreadsheets/_upload_file_step.erb", "app/views/importable/spreadsheets/_upload_file_step.erb"
-      copy_file "spreadsheets/new.html.erb", "app/views/importable/spreadsheets/new.html.erb"
-      copy_file "spreadsheets/show.html.erb", "app/views/importable/spreadsheets/show.html.erb"
+
+      files = %w[
+        _actions
+        _choose_worksheet_step
+        _errors
+        _upload_file_step
+        new
+        show
+      ]
+
+      namespace = "#{name}/" unless name.blank?
+
+      files.each do |file|
+        from_path = "spreadsheets/#{file}.html.erb"
+        to_path = "app/views/importable/spreadsheets/#{namespace}#{file}.html.erb"
+        copy_file from_path, to_path
+      end
     end
   end
 end
